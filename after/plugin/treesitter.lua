37947ba846766384dbbf66610ddb971b57957767
Rodrigo Aldegheri <rodrigo.aldegheri@hotmail.com>
2024-11-11 20:46:35 +0100

    poche cose

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "c", "lua", "vim", "vimdoc", "query",  "javascript", "typescript" },
+  ensure_installed = { "c", "lua", "vim", "vimdoc", "query",  "javascript", "typescript", "python"},
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
@@ -11,6 +11,5 @@ require'nvim-treesitter.configs'.setup {
 
   highlight = {
     enable = true,
-    additional_vim_regex_highlighting = false,
   },
 }
